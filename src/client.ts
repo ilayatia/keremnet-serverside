54647b00552ad12f887880219e3f722f8f59f2f5
ilay atia <ilayatia@gmail.com>
2025-06-26 14:39:25 +0300

    better client

--- src/client.ts
+++ src/client.ts
@@ -1,15 +1,24 @@
+import { CommentModel } from "./Models/Comment";
+
 const { response } = require("express");
 
-fetch("http://localhost:8080/api/posts")
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+const fetchData = async (url: string) => {
+  const response = await fetch(url);
+  const json = await response.json();
+  console.log(json);
+};
 
-fetch("http://localhost:8080/api/comment/1", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({ text: "30", name: "30" }),
-})
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+const postComment = async (url: string, data: CommentModel) => {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  const json = await response.json();
+  console.log(json);
+};
+
+postComment("http://localhost:8080/api/comment/1", { text: "30", name: "30" });
+fetchData("http://localhost:8080/api/posts");
