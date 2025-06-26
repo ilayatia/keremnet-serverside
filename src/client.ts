ff02f94c82b8928db89a53dc2d4e17f04765c99a
ilay atia <ilayatia@gmail.com>
2025-06-26 15:11:18 +0300

    way better api call with async await

--- src/client.ts
+++ src/client.ts
@@ -4,18 +4,19 @@ import { PostModel } from "./Models/Post";
 const { response } = require("express");
 
 const getPosts = async (url: string) => {
-  fetch(url)
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+  const response = await fetch(url);
+  const json = await response.json();
+  return json;
 };
-const postComment = (url: string, data: CommentModel) => {
-  fetch(url, {
+const postComment = async (url: string, data: CommentModel) => {
+  const response = await fetch(url, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(data),
-  })
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+  });
+  const json =await response.json();
+  return json;
 };
+getPosts("http://localhost:8080/api/posts").then(response=>console.log(response))
