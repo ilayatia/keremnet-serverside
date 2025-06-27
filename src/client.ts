97f96087a1cc357516bff1452a9638503a82d999
ilay atia <ilayatia@gmail.com>
2025-06-27 10:10:29 +0300

    added post method to send new posts to server

--- src/client.ts
+++ src/client.ts
@@ -17,3 +17,17 @@ const postComment = async (url: string, data: CommentModel) => {
   const json =await response.json();
   return json;
 };
+const addPostToserver = async (url: string, data: PostToPostToServer) => {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  const json =await response.json();
+  return json;
+};
+//  getPosts("http://localhost:8080/api/posts").then(out=>console.log(out))
+// addPostToserver("http://localhost:8080/api/addpost",{date:"12/13/24",likes:35,name:"goo",text:"adsfds"}).then(out=>console.log(out))
+ //getPosts("http://localhost:8080/api/posts").then(out=>console.log(out))
--- src/controllers.ts
+++ src/controllers.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import { getAllPosts, getPostById, postComment } from "./services";
+import { addPost, getAllPosts, getPostById, postComment } from "./services";
 
 const express = require("express");
 const app = express();
@@ -15,6 +15,8 @@ app.get("/api/post/:id", getPostById);
 app.get("/api/posts", getAllPosts);
 
 app.post("/api/comment/:id", postComment);
+
+app.post("/api/addpost", addPost)
 
 app.listen(port, () => {
   console.log("server is up and listening at port " + port);
